a33f13752be27db44840203290f6445d7c89b24e
Dan Sackett <danesackett@gmail.com>
2015-08-15 00:42:32 -0400

    Leaner code through more generic functions and test suite for singly linked list done in goconvey

--- data-structures/linked-list/singlylinkedlist.go
+++ data-structures/linked-list/singlylinkedlist.go
@@ -7,7 +7,7 @@ type SingleNode struct {
 	// Value stored for the node
 	Value interface{}
 	// Next item is a SingleNode which follows this one
-	Next *SingleNode
+	next *SingleNode
 }
 
 // Returns an initialized SingleNode
@@ -17,8 +17,8 @@ func NewSingleNode(v interface{}) *SingleNode {
 
 // Returns the item that follows a node
 func (n *SingleNode) Next() *SingleNode {
-	if n.Next {
-		return n.Next
+	if n.next != nil {
+		return n.next
 	}
 	return nil
 }
@@ -41,29 +41,42 @@ func Check(v1 interface{}, v2 interface{}) bool {
 	return v1 == v2
 }
 
+// Function which takes variadic args and returns a new list of args to slice
+func Slice(args ...interface{}) []interface{} {
+	return args
+}
+
 // Convenience funtion for finding if a node exists based on an arbitrary type
 // check. Could check by an index or value to see if it exists in the list.
 func (l *SingleList) _FindPrevBy(v interface{}, t string) (bool, int, *SingleNode) {
 	if l.IsEmpty() {
 		return false, -1, nil
 	}
-	prev := nil
+
+	var prev *SingleNode
+	prev = nil
 	cur_idx := 0
-	for x := l.Root; x != nil; x = x.Next() {
+
+	for x := l.GetFirst(); x != nil; x = x.Next() {
 		if (t == "index" && Check(cur_idx, v)) || (t == "value" && Check(x.Value, v)) {
 			return true, cur_idx, prev
 		}
 	}
+
 	return false, -1, nil
 }
 
 // Wrapper function to find list data based on an index
 func (l *SingleList) _FindPrevByIndex(idx int) (bool, int, *SingleNode) {
+	if idx > l.Size()-1 {
+		return false, -1, nil
+	}
+
 	return l._FindPrevBy(idx, "index")
 }
 
 // Wrapper function to find list data based on a value
-func (l *SingleList) _FindPrevByValue(v interface{}) (bool, *SingleNode) {
+func (l *SingleList) _FindPrevByValue(v interface{}) (bool, int, *SingleNode) {
 	return l._FindPrevBy(v, "value")
 }
 
@@ -71,45 +84,57 @@ func (l *SingleList) _FindPrevByValue(v interface{}) (bool, *SingleNode) {
 // node and a value to insert
 func (l *SingleList) _AddAfterNode(prev *SingleNode, v interface{}) *SingleNode {
 	new_node := NewSingleNode(v)
+
 	if prev == nil {
-		new_node.Next = l.Root
+		new_node.next = l.GetFirst()
 		l.Root = new_node
 	} else if prev.Next().Next() == nil {
-		prev.Next.Next = new_node
+		prev.next.next = new_node
 	} else {
-		new_node.Next = prev.Next()
-		prev.Next = new_node
+		new_node.next = prev.Next()
+		prev.next = new_node
 	}
+
 	l.Length++
+
 	return new_node
 }
 
 // Convenience method for removing a node after another. It accepts a current node.
 func (l *SingleList) _RemoveAfterNode(prev *SingleNode) *SingleNode {
+	var old_node *SingleNode
+
 	if prev == nil {
-		l.Root = l.Root.Next()
+		old_node = l.GetFirst()
+		l.Root = l.GetFirst().Next()
 	} else if prev.Next().Next() == nil {
-		prev.Next = nil
+		old_node = l.GetLast()
+		prev.next = nil
 	} else {
-		prev.Next = prev.Next().Next()
+		old_node = prev.Next()
+		prev.next = prev.Next().Next()
 	}
+
 	l.Length--
-	return new_node
+
+	return old_node
 }
 
 // Based on an index, insert a new value into the list
 func (l *SingleList) Insert(idx int, v interface{}) bool {
 	exists, _, prev := l._FindPrevByIndex(idx)
+
 	if exists {
 		l._AddAfterNode(prev, v)
 		return true
 	}
+
 	return false
 }
 
 // Add a new value to the end of the list
-func (l *SingleList) Append(v interface{}) {
-	l._AddAfterNode(l.GetLast(), v)
+func (l *SingleList) Append(v interface{}) *SingleNode {
+	return l._AddAfterNode(l.GetLast(), v)
 }
 
 // With a slice of values, add each to the end of the list
@@ -120,20 +145,24 @@ func (l *SingleList) AppendMultiple(values []interface{}) {
 }
 
 // Add a new value to the beginning of the list
-func (l *SingleList) Prepend(v interface{}) {
-	l._AddAfterNode(l.GetFirst(), v)
+func (l *SingleList) Prepend(v interface{}) *SingleNode {
+	return l._AddAfterNode(l.GetFirst(), v)
 }
 
 // Check if a value is currently in the list
 func (l *SingleList) Contains(v interface{}) bool {
-	exists, _, _ := l._FindPrevByValue(v)
-	return exists
+	return Slice(l._FindPrevByValue(v))[0].(bool)
 }
 
 // Get a node from the list based on its index
 func (l *SingleList) Get(idx int) *SingleNode {
-	_, _, prev := l._FindPrevByIndex(idx)
-	return prev.Next()
+	n := Slice(l._FindPrevByIndex(idx))[2].(*SingleNode)
+
+	if n != nil {
+		return n.Next()
+	}
+
+	return l.GetFirst()
 }
 
 // Get the first node in the list (root)
@@ -143,13 +172,15 @@ func (l *SingleList) GetFirst() *SingleNode {
 
 // Get the last node in the list
 func (l *SingleList) GetLast() *SingleNode {
+	if l.IsEmpty() {
+		return nil
+	}
 	return l.Get(l.Size() - 1)
 }
 
 // Get the first index of a given value
 func (l *SingleList) IndexOf(v interface{}) int {
-	_, idx, _ := l._FindPrevByValue(v)
-	return idx
+	return Slice(l._FindPrevByValue(v))[1].(int)
 }
 
 // Get the last index of a given value
@@ -157,21 +188,23 @@ func (l *SingleList) LastIndexOf(v interface{}) int {
 	if l.IsEmpty() {
 		return -1
 	}
-	cur_idx = 0
-	prev_idx = -1
-	for n := l.Root; n != nil; n.Next() {
-		if n.Value == v {
+
+	cur_idx := 0
+	prev_idx := -1
+
+	for n := l.GetFirst(); n != nil; n.Next() {
+		if Check(n.Value, v) {
 			prev_idx = cur_idx
 		}
-		cur_idx += 1
+		cur_idx++
 	}
+
 	return prev_idx
 }
 
 // Remove the first match for the given value
-func (l *SingleList) Remove(v interface{}) {
-	exists, _, prev := l._FindPrevByValue(v)
-	l._RemoveAfterNode(prev)
+func (l *SingleList) Remove(v interface{}) *SingleNode {
+	return l._RemoveAfterNode(Slice(l._FindPrevByValue(v))[2].(*SingleNode))
 }
 
 // Return and remove the last node in the list
@@ -196,15 +229,17 @@ func (l *SingleList) Size() int {
 
 // Check if the list is empty
 func (l *SingleList) IsEmpty() bool {
-	return l.Length == 0
+	return l.Size() == 0
 }
 
 // Convert the list to a slice
 func (l *SingleList) ToSlice() []interface{} {
-	s := make([]interface{})
+	s := make([]interface{}, l.Size())
 
-	for n := l.Root; n != nil; n = n.Next() {
-		append(s, n)
+	ctr := 0
+	for n := l.GetFirst(); n != nil; n = n.Next() {
+		s[ctr] = n
+		ctr++
 	}
 
 	return s
--- data-structures/linked-list/singlylinkedlist_test.go
+++ data-structures/linked-list/singlylinkedlist_test.go
@@ -15,4 +15,112 @@ func TestSinglyLinkedList(t *testing.T) {
 			So(empty.IsEmpty(), ShouldBeTrue)
 		})
 	})
+	Convey("Given a Singly Linked List with one node", t, func() {
+		single := NewSingleList()
+		single.Append(7)
+		Convey("it has size of one", func() {
+			So(single.Size(), ShouldEqual, 1)
+		})
+		Convey("it is not empty", func() {
+			So(single.IsEmpty(), ShouldBeFalse)
+		})
+		Convey("it contains the node", func() {
+			So(single.Contains(7), ShouldBeTrue)
+		})
+		Convey("it has a root of 7", func() {
+			So(single.GetFirst().Value, ShouldEqual, 7)
+		})
+		Convey("it is also the last node", func() {
+			So(single.GetLast().Value, ShouldEqual, 7)
+		})
+		Convey("its first node is at index 0", func() {
+			So(single.Get(0).Value, ShouldEqual, 7)
+			So(single.IndexOf(7), ShouldEqual, 0)
+			So(single.LastIndexOf(7), ShouldEqual, 0)
+		})
+		Convey("its value can be updated", func() {
+			single.Set(0, 10)
+			So(single.Get(0).Value, ShouldEqual, 10)
+			So(single.IndexOf(10), ShouldEqual, 0)
+			So(single.IndexOf(7), ShouldEqual, -1)
+			So(single.LastIndexOf(7), ShouldEqual, -1)
+		})
+		Convey("it can be removed", func() {
+			single.Remove(10)
+			So(single.IndexOf(10), ShouldEqual, -1)
+			So(single.GetFirst(), ShouldEqual, nil)
+		})
+	})
+	Convey("Given a Singly Linked List with multiple nodes", t, func() {
+		multiple := NewSingleList()
+
+		data := []int{1, 3, 5, 7, 9}
+		vals := make([]interface{}, len(data))
+
+		for i, d := range data {
+			vals[i] = d
+		}
+
+		multiple.AppendMultiple(vals)
+
+		Convey("it has size of 5", func() {
+			So(multiple.Size(), ShouldEqual, 5)
+		})
+		Convey("its root is 1", func() {
+			So(multiple.GetFirst().Value, ShouldEqual, 1)
+		})
+		Convey("its tail is 9", func() {
+			So(multiple.GetLast().Value, ShouldEqual, 9)
+		})
+		Convey("it can be prepended", func() {
+			multiple.Prepend(9)
+			So(multiple.GetFirst().Value, ShouldEqual, 9)
+			So(multiple.Size(), ShouldEqual, 6)
+		})
+		Convey("it contains our values", func() {
+			So(multiple.Contains(1), ShouldBeTrue)
+			So(multiple.Contains(3), ShouldBeTrue)
+			So(multiple.Contains(5), ShouldBeTrue)
+			So(multiple.Contains(7), ShouldBeTrue)
+			So(multiple.Contains(9), ShouldBeTrue)
+		})
+		Convey("it can determine indexes correctly", func() {
+			So(multiple.IndexOf(9), ShouldEqual, 0)
+			So(multiple.IndexOf(1), ShouldEqual, 1)
+			So(multiple.IndexOf(3), ShouldEqual, 2)
+			So(multiple.IndexOf(5), ShouldEqual, 3)
+			So(multiple.IndexOf(7), ShouldEqual, 4)
+			So(multiple.LastIndexOf(9), ShouldEqual, 5)
+		})
+		Convey("it can remove values", func() {
+			So(multiple.Pop().Value, ShouldEqual, 9)
+			So(multiple.GetLast().Value, ShouldEqual, 7)
+			So(multiple.Contains(9), ShouldBeTrue)
+			So(multiple.LeftPop().Value, ShouldEqual, 9)
+			So(multiple.GetFirst().Value, ShouldEqual, 1)
+			So(multiple.Contains(9), ShouldBeFalse)
+			So(multiple.Remove(5).Value, ShouldEqual, 5)
+			So(multiple.Contains(5), ShouldBeFalse)
+		})
+		Convey("it can set values", func() {
+			multiple.Set(0, 99)
+			multiple.Set(1, 999)
+			multiple.Set(2, 9999)
+			So(multiple.GetFirst().Value, ShouldEqual, 99)
+			So(multiple.Get(1).Value, ShouldEqual, 999)
+			So(multiple.GetLast().Value, ShouldEqual, 9999)
+		})
+		Convey("it can be converted to a slice", func() {
+			s := multiple.ToSlice()
+
+			data := []int{99, 999, 9999}
+			vals := make([]interface{}, len(data))
+
+			for i, d := range data {
+				vals[i] = d
+			}
+
+			So(s, ShouldEqual, vals)
+		})
+	})
 }
